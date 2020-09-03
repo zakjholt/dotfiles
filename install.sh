ee9cf7faa07e382ddec5bb9ec0a0a8843113de4e
Zak <zak.j.holt@gmail.com>
2020-09-03 09:38:52 -0400

    add fish conf and starship conf

--- install.sh
+++ install.sh
@@ -2,24 +2,33 @@
 
 DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+backup_existing(){
+  file=$1
+
+  if test -f $file; then
+    echo "$file" already exists. Moving to "$file".bak
+    mv "$file" "$file".bak
+  fi
+}
+
 declare -a HomeDotfiles=(".hyper.js" ".tmux.conf" ".zshrc")
 
 for filename in ${HomeDotfiles[@]}; do
   echo Installing "$filename"...
 
-  if test -f ~/"$filename"; then
-    echo "$filename already exists. Moving to ~/$filename.bak"
-    mv ~/"$filename" ~/"filename".bak
-  fi
+  backup_existing ~/"$filename"
   ln -s "$DOTFILE_DIR"/"$filename" ~/"$filename"
 done
 
 ## Link init.vim
-if test -f ~/.config/nvim/init.vim; then
-  echo init.vim already exists. Moving to ~/.config/nvim/init.vim.bak
-  mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
-fi
-
+backup_existing ~/.config/nvim/init.vim
 ln -s "$DOTFILE_DIR"/init.vim ~/.config/nvim/init.vim
 
+## Link config.fish
+backup_existing ~/.config/fish/config.fish
+ln -s "$DOTFILE_DIR"/config.fish ~/.config/fish/config.fish
+
+## Link starship.toml
+backup_existing ~/.config/starship.toml
+ln -s "$DOTFILE_DIR/starship.toml" ~/.config/starship.toml
 
