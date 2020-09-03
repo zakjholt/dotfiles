ae8297c6c136a4901bd4b214f0e0e47f6771ca54
Zak <zak.j.holt@gmail.com>
2020-09-03 08:57:58 -0400

    add .bak logic

--- install.sh
+++ install.sh
@@ -6,11 +6,20 @@ declare -a HomeDotfiles=(".hyper.js" ".tmux.conf" ".zshrc")
 
 for filename in ${HomeDotfiles[@]}; do
   echo Installing "$filename"...
-  rm -rf ~/"$filename"
+
+  if test -f ~/"$filename"; then
+    echo "$filename already exists. Moving to ~/$filename.bak"
+    mv ~/"$filename" ~/"filename".bak
+  fi
   ln -s "$DOTFILE_DIR"/"$filename" ~/"$filename"
 done
 
 ## Link init.vim
+if test -f ~/.config/nvim/init.vim; then
+  echo init.vim already exists. Moving to ~/.config/nvim/init.vim.bak
+  mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+fi
+
 ln -s "$DOTFILE_DIR"/init.vim ~/.config/nvim/init.vim
 
 
